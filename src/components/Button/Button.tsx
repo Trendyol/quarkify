786715a5220beb05216ef39f48e7d49b8da1db57
yavuzkoca <ykoca0808@gmail.com>
2019-06-27 09:08:19 +0300

    Add template of button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Button extends React.Component<any, any> {
+    public render() {
+        return (
+            <button>Sa</button>
+        );
+    }
+}
+
+export default Button;
