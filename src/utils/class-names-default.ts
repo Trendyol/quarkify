1d4fac074b28489651c765c7a3c056d54eac74ae
yavuzkoca <ykoca0808@gmail.com>
2019-06-27 11:04:03 +0300

    Add classNamesDefault for auto variant declarations

---
+++ src/utils/class-names-default.ts
@@ -0,0 +1,12 @@
+import classNames from "classnames";
+
+const classNamesDefault = (props: any) => {
+    return classNames(
+        props.className,
+        props.primary && 'primary',
+        props.secondary && 'secondary',
+        props.disabled && 'disabled',
+    );
+};
+
+export default classNamesDefault;
