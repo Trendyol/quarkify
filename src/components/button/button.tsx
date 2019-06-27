e09121d95ceaf6113f671e45618787c3a98886de
yavuzkoca <ykoca0808@gmail.com>
2019-06-27 11:01:24 +0300

    Remove default value rule in tslint

---
+++ src/components/button/button.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import IVariant from "../../interfaces/IVariant";
+import '../../styles/components/_button.scss';
+import classNamesDefault from '../../utils/class-names-default';
+
+class Button extends React.Component<IProps, any> {
+    public render() {
+        return (
+            <button
+                className={classNamesDefault(this.props)}
+                disabled={this.props.disabled}
+                onClick={this.a}
+            >
+                {this.props.children}
+            </button>
+        );
+    }
+    private a() {
+        alert('sa');
+    }
+}
+
+interface IProps extends IVariant {
+    disabled?: boolean;
+    className?: string;
+    width?: string;
+    height?: string;
+    onClick?: () => {};
+}
+
+export default Button;
