55518dfb3cc350ffed813b134a82d0f3daec321f
yavuzkoca <ykoca0808@gmail.com>
2019-06-28 15:11:14 +0300

    Initiliaze popup test

---
+++ src/components/popup/test/popup.spec.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Button from "../button";
+import faker from "faker";
+import sinon from "sinon";
+import Popup from '../index';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("popup specs", () => {
+  const sandbox = sinon.createSandbox();
+
+  afterEach(() => {
+    sandbox.verifyAndRestore();
+  });
+
+  it("should render popup component", () => {
+    const wrapper = shallow(<Popup show={true} />);
+  });
+});
