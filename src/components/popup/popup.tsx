54fa78e5e6418c536319898a2a5995a6d97c2bb4
InanBerkin <berkin.inan1@gmail.com>
2019-06-28 15:08:16 +0300

    Add basic popup features

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -1,9 +1,30 @@
+import classNames from "classnames";
 import React from "react";
-import "../../styles/components/_button.scss";
-import classNamesDefault from "../../utils/class-names-default";
+import "../../styles/components/_popup.scss";
+import Button from "../button";
 
-class Popup extends React.PureComponent<IProps> {}
+class Popup extends React.PureComponent<IProps> {
+  private classes = classNames("popup");
+  public render() {
+    if (!this.props.show) {
+      return null;
+    }
+    return (
+      <div className="popup-overlay">
+        <section className="popup-main">
+          <h1>Modal</h1>
+          <Button onClick={this.showAlert}>Click me</Button>
+        </section>
+      </div>
+    );
+  }
+  private showAlert = () => {
+    alert(1);
+  };
+}
 
-interface IProps {}
+interface IProps {
+  show: boolean;
+}
 
 export default Popup;
