1278fb5d61ced21bcc1cacdc52b6165723895b0c
yavuzkoca <ykoca0808@gmail.com>
2019-06-28 10:27:25 +0300

    Fix typo in default props

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -4,8 +4,8 @@ import classNamesDefault from "../../utils/class-names-default";
 
 class Input extends React.PureComponent<IProps> {
   private static defaultProps = {
-    type: "primary",
-    variant: "text",
+    type: "text",
+    variant: "primary",
   };
 
   public render() {
