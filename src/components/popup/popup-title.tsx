36099f0ffdf0528466fadf086db72a6c4d1bb175
yavuzkoca <ykoca0808@gmail.com>
2019-07-01 11:51:26 +0300

    Add style of close icon popup component

--- src/components/popup/popup-title.tsx
+++ src/components/popup/popup-title.tsx
@@ -1,9 +1,18 @@
+import classNames from "classnames";
 import * as React from "react";
 
 function Title(props: any) {
+  const classes = classNames(
+    props.iconLeft && "popup-icon-left",
+    "icon-close",
+    "popup-icon-position",
+  );
   return (
     <div>
-      <div className="popup-x-icon">x</div>
+      <i className={classes}/>
+      <div>
+        {props.children}
+      </div>
     </div>
   );
 }
