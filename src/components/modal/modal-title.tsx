0a55c21c9055d1b3aae444ff1371079d06e0b11f
antkaynak <antkaynak1@gmail.com>
2019-07-02 11:40:01 +0300

    Refactor old function definition to arrow function in modal component

--- src/components/modal/modal-title.tsx
+++ src/components/modal/modal-title.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 
-function Title(props: any) {
+const Title = (props: any) => {
   return (
     <div className="modal-title">
       <div>{props.children}</div>
     </div>
   );
-}
+};
 
 export default Title;
