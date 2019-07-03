a87f8e3558c3bbae07c9c45d5d22751bd273ce2f
InanBerkin <berkin.inan1@gmail.com>
2019-07-03 11:07:14 +0300

    Add popup overlay click onClose prop

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -2,31 +2,40 @@ import classNames from "classnames";
 import React, { ReactNode } from "react";
 import ReactDOM from "react-dom";
 import "../../styles/components/_popup.scss";
+import Icon from "../icon";
 
-const insideClick = (event: any) => {
-  event.stopPropagation();
-};
-
-function Popup({ show, children, onClose, iconLeft }: IProps) {
+const Popup = ({ show, children, onClose, iconLeft, noIcon = false, closeOnOverlayClick = true }: IProps) => {
   const popupMainClasses = classNames("popup-main", "slideInUp");
   const popupIconClasses = classNames(
     iconLeft && "popup-icon-left",
     "icon-close",
     "popup-icon-position",
   );
+
   if (!show) {
     return null;
   }
+
+  const popupBodyClick = (event: React.SyntheticEvent) => {
+    event.stopPropagation();
+  };
+
+  const overlayClick = () => {
+    if (closeOnOverlayClick) {
+      onClose();
+    }
+  };
+
   return ReactDOM.createPortal(
-    <div className={"popup-overlay"} onClick={onClose}>
-      <div className={popupMainClasses} onClick={insideClick}>
-        <i className={popupIconClasses} onClick={onClose}/>
+    <div className="popup-overlay" onClick={overlayClick}>
+      <div className={popupMainClasses} onClick={popupBodyClick}>
+        {!noIcon && <Icon className={popupIconClasses} onClick={onClose} name="close"/>}
         {children}
       </div>
     </div>,
     document.body,
   );
-}
+};
 
 interface IProps {
   show: boolean;
@@ -34,6 +43,8 @@ interface IProps {
   slideIn?: string;
   onClose: () => void;
   iconLeft?: boolean;
+  noIcon?: boolean;
+  closeOnOverlayClick?: boolean;
 }
 
 export default Popup;
