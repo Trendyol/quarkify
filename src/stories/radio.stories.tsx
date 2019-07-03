0e389d2e85bd52d4838c08019980857fef94ecf1
antkaynak <antkaynak1@gmail.com>
2019-07-03 16:08:02 +0300

    Fix display bug on radio story

--- src/stories/radio.stories.tsx
+++ src/stories/radio.stories.tsx
@@ -4,12 +4,12 @@ import Radio from "../components/radio";
 
 const stories = storiesOf("Radio", module);
 
-stories.add("Radio", () => (
+stories.add("Default", () => (
   <div>
       <form>
-          <Radio name={"gender"} value={"male"} checked label={"Male"}/>
-          <Radio name={"gender"} value={"female"} label={"Female"}/>
-          <Radio name={"gender"} value={"other"} label={"Other"}/>
+        <Radio name={"gender"} value={"female"} label={"Female"}/>
+        <Radio name={"gender"} value={"male"} label={"Male"}/>
+        <Radio name={"gender"} value={"other"} label={"Other"}/>
       </form>
   </div>
 ));
