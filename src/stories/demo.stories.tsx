221a022278c34fc06a0b5a3561529f4414cebdb5
yavuzkoca <ykoca0808@gmail.com>
2019-07-10 11:39:09 +0300

    Add product-info footer for demo

--- src/stories/demo.stories.tsx
+++ src/stories/demo.stories.tsx
@@ -1,10 +1,29 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
-import "./demo.css";
+import Box from "../components/box";
 import Button from "../components/button";
 import Icon from "../components/icon";
+import Select from "../components/select";
+import "./demo.css";
 
 const stories = storiesOf("Demo", module);
+const items = [
+  {
+    id: 1,
+    name: "XS",
+    variant: "primary",
+  },
+  {
+    id: 2,
+    name: "S",
+    variant: "primary",
+  },
+  {
+    id: 3,
+    name: "M",
+    variant: "primary",
+  },
+];
 
 stories.add("First Page", () => (
   <div>
@@ -49,6 +68,72 @@ stories.add("First Page", () => (
             <span className="product-info-delivery__normal__time">11 - 16 Temmuz</span>
           </div>
         </span>
+      </div>
+      <div className={"product-info__size"}>
+        <div className={"product-info__size-name"}>
+          Beden
+        </div>
+        <div className={"product-info__size-name__table"}>
+          Beden Tablosu
+        </div>
+      </div>
+      <div className={"product-info__size-select"}>
+        <Select items={items} fluid/>
+      </div>
+      <div className={"product-info__information"}>
+        <h3 className={"description__header__title"}>Ürün Bilgileri</h3>
+        <div className={"description__body"}>
+          <ul>
+            <li data-free-returns="true">15 gün içerisinde ücretsiz iade. Detaylı bilgi için
+              <span
+                className="description__body__return_conditions"
+              >
+                Tıklayın.
+              </span>
+            </li>
+            <li data-market-place-supplier="true">Bu ürün Trendyol adına
+              <span
+                className="description__body__merchant"
+              >
+                AVVA
+              </span>tarafından gönderilecektir.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={"product-info__promotion"}>
+        <h3 className={"description__header__title"}>Avva - Alışveriş Fırsatı</h3>
+        <div className={"product-info__promotion-body"}>
+          <div className={"product-info__promotion-item"}>
+            <Icon name={"time"} style={{ color: "#b00000" }}/>
+            <span className={"product-info__promotion-item__label"} style={{ color: "#b00000" }}>Kalan Süre: </span>
+            <span className={"product-info__promotion-item__label-name"}>&nbsp;&nbsp;1 gün 18:35:04</span>
+          </div>
+          <div className={"product-info__promotion-item"}>
+            <Icon name={"info-tag"} style={{ color: "#f27a1a" }}/>
+            <span className={"product-info__promotion-item__label"} style={{ color: "#f27a1a" }}>Kampanya: </span>
+            <span className={"product-info__promotion-item__label-name"}>&nbsp;&nbsp;Sepette %60 İndirim</span>
+          </div>
+          <div className={"product-info__promotion-item"}>
+            <Icon name={"info-tag"} style={{ color: "#f27a1a" }}/>
+            <span className={"product-info__promotion-item__label"} style={{ color: "#f27a1a" }}>Kampanya: </span>
+            <span className={"product-info__promotion-item__label-name"}>&nbsp;&nbsp;Kargo Bedava</span>
+          </div>
+        </div>
+      </div>
+      <div className={"more-buttons"}>
+        <Box>
+          <div className={"more-buttons__item"}>
+            <Icon name={"forward-button"} style={{ color: "#f27a1a" }}/>
+            <span className="redirection-box__text">Daha Fazla Avva</span>
+          </div>
+        </Box>
+        <Box>
+          <div className={"more-buttons__item"}>
+            <Icon name={"forward-button"} style={{ color: "#f27a1a" }}/>
+            <span className="redirection-box__text">Daha Fazla Pantolon</span>
+          </div>
+        </Box>
       </div>
     </div>
 
