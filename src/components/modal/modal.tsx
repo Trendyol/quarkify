35c88db30201e5ca14eac7358f9c225d309f2c25
InanBerkin <berkin.inan1@gmail.com>
2019-07-10 13:49:40 +0300

    Fix modal document body scroll lock

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -6,27 +6,53 @@ import Actions from "./modal-actions";
 import Content from "./modal-content";
 import Header from "./modal-header";
 
-const Modal = ({ show, children, animation = "slideInRight" }: IModalProps) => {
-  return (
-    <CSSTransition
-      in={show}
-      unmountOnExit
-      timeout={300}
-      classNames={`${animation} modal`}
-    >
-      <div className="modal-main">{children}</div>
-    </CSSTransition>
-  );
-};
+class Modal extends React.Component<IModalProps> {
+  public static Header = Header;
+  public static Actions = Actions;
+  public static Content = Content;
+
+  public componentDidMount(): void {
+    if (this.props.show && typeof window !== "undefined") {
+      document.body.classList.add("disable-scroll");
+    }
+  }
+
+  public shouldComponentUpdate(
+    nextProps: Readonly<IModalProps>,
+    nextState: Readonly<{}>,
+    nextContext: any,
+  ): boolean {
+    if (nextProps.show !== this.props.show && this.props.onChange) {
+      this.props.onChange();
+    }
+    if (nextProps.show && typeof window !== "undefined") {
+      document.body.classList.add("disable-scroll");
+    } else if (typeof window !== "undefined") {
+      document.body.classList.remove("disable-scroll");
+    }
+    return true;
+  }
+
+  public render() {
+    const { show, children, animation = "slideInRight" } = this.props;
+    return (
+      <CSSTransition
+        in={show}
+        unmountOnExit
+        timeout={300}
+        classNames={`${animation} modal`}
+      >
+        <div className="modal-main">{children}</div>
+      </CSSTransition>
+    );
+  }
+}
 
 interface IModalProps {
   show: boolean;
   animation?: animationTypes;
   children?: ReactNode;
+  onChange?: () => void;
 }
-
-Modal.Header = Header;
-Modal.Actions = Actions;
-Modal.Content = Content;
 
 export default Modal;
