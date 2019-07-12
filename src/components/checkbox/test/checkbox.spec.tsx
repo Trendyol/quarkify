e1f8ed1cb3c9e28bb839441b7bcb10136c867dc9
antkaynak <antkaynak1@gmail.com>
2019-07-12 12:32:43 +0300

    Add className prop to checkbox component

--- src/components/checkbox/test/checkbox.spec.tsx
+++ src/components/checkbox/test/checkbox.spec.tsx
@@ -15,7 +15,7 @@ describe("checkbox specs", () => {
   });
 
   it("should render checkbox component", () => {
-    const wrapper = shallow(<CheckBox />);
+    const wrapper = shallow(<CheckBox value={"test"}/>);
 
     expect(wrapper).toHaveLength(1);
   });
@@ -23,7 +23,7 @@ describe("checkbox specs", () => {
   it("should call onChange function ", () => {
     const spy = sandbox.spy();
     const text = faker.random.word();
-    const wrapper = shallow(<CheckBox onChange={spy} />);
+    const wrapper = shallow(<CheckBox value={"test"} onChange={spy} />);
     const event = { target: { value: text } };
 
     wrapper.find("input").simulate("change", event);
@@ -34,7 +34,7 @@ describe("checkbox specs", () => {
   it("should not call callback function when changed and input is disabled", () => {
     const spy = sandbox.spy();
     const text = faker.random.word();
-    const wrapper = shallow(<CheckBox disabled />);
+    const wrapper = shallow(<CheckBox value={"test"} disabled />);
     const event = { target: { value: text } };
 
     wrapper.simulate("change", event);
@@ -44,26 +44,33 @@ describe("checkbox specs", () => {
 
   it("should render a label", () => {
     const text = faker.random.word();
-    const wrapper = shallow(<CheckBox label={text} />);
+    const wrapper = shallow(<CheckBox value={"test"} label={text} />);
 
     expect(wrapper.find("label")).toHaveLength(1);
   });
 
   it("should accept additional classNames", () => {
     const testClass = faker.lorem.word();
-    const wrapper = shallow(<CheckBox className={testClass} />);
+    const wrapper = shallow(<CheckBox value={"test"} className={testClass} />);
     expect(wrapper.find(`.${testClass}`)).toHaveLength(1);
   });
 
   it("should convert text type prop to checkbox type prop", () => {
-    const wrapper = shallow(<CheckBox type="text" />);
+    const wrapper = shallow(<CheckBox value={"test"} type="text" />);
     const inputElement = wrapper.find("input");
     expect(inputElement.prop("type")).toBe("checkbox");
   });
 
   it("should have error className when error prop is passed", () => {
-    const wrapper = shallow(<CheckBox error />);
+    const wrapper = shallow(<CheckBox value={"test"} error />);
 
     expect(wrapper.hasClass("error")).toBe(true);
+  });
+
+  it("should accept additional classNames", () => {
+    const fakeClass = faker.lorem.word();
+    const wrapper = shallow(<CheckBox value={"test"} className={fakeClass}/>);
+
+    expect(wrapper.hasClass(fakeClass)).toBe(true);
   });
 });
