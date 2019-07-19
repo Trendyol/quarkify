af7ebd9cdb8b68542576fe515c74a344908aaa96
InanBerkin <berkin.inan1@gmail.com>
2019-07-19 10:05:08 +0300

    Changed default props to default values in destructuring

--- src/components/modal/modal-header.tsx
+++ src/components/modal/modal-header.tsx
@@ -1,25 +1,29 @@
-import React, { FunctionComponent } from "react";
+import classNames from "classnames";
+import React, { PureComponent } from "react";
 import Icon from "../icon";
 
-const Header: FunctionComponent<IHeaderProps> = ({
-  children,
-  rightIcon,
-  leftIcon,
-  leftIconOnClick,
-  rightIconOnClick,
-}) => {
-  const classNames = ["q-modal-header", leftIcon && "q-has-left-icon"];
-  return (
-    <div className={classNames.join(" ")}>
-      {leftIcon &&
-      <Icon className={"q-i"} aria-label={leftIcon} name={leftIcon} onClick={leftIconOnClick} />}
-      <h1 className="q-modal-title">{children}</h1>
-      {rightIcon &&
-        <Icon className={"q-i"} aria-label={rightIcon} name={rightIcon} onClick={rightIconOnClick} />
-      }
-    </div>
-  );
-};
+class Header extends PureComponent<IHeaderProps> {
+  public render() {
+      const {
+        children,
+        rightIcon,
+        leftIcon,
+        leftIconOnClick,
+        rightIconOnClick,
+      } = this.props;
+      const headerClasses = classNames("q-modal-header", leftIcon && "q-has-left-icon");
+      return (
+        <div className={headerClasses}>
+          {leftIcon &&
+          <Icon className={"q-i"} aria-label={leftIcon} name={leftIcon} onClick={leftIconOnClick} />}
+          <h1 className="q-modal-title">{children}</h1>
+          {rightIcon &&
+          <Icon className={"q-i"} aria-label={rightIcon} name={rightIcon} onClick={rightIconOnClick} />
+          }
+        </div>
+      );
+    }
+  }
 
 export interface IHeaderProps {
   leftIcon?: string;
