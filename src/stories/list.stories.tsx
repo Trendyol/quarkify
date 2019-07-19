0c98ce13cd69107a935c267e1bf2fc8fe4618d68
antkaynak <antkaynak1@gmail.com>
2019-07-19 11:21:22 +0300

    Add new story into list module

--- src/stories/list.stories.tsx
+++ src/stories/list.stories.tsx
@@ -1,25 +1,33 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
-import Button from "../components/button";
-import Item from "../components/item";
 import List from "../components/list";
+import ListItem from "../components/list/listItem";
 
 const stories = storiesOf("List", module);
 
 stories.add("Default", () => (
   <List>
-    <Item>15 gün içerisinde ücretsiz iade. Detaylı bilgi için.Tıklayın.</Item>
-    <Item>24 saatte kargoda fırsatı iş günlerinde geçerlidir.</Item>
-    <Item>Modelin Ölçüleri: Boy: 1.75, Göğüs: 80, Bel: 60, Kalça: 88</Item>
-    <Item>Mankenin üzerindeki ürün S/36 bedendir.</Item>
+    <ListItem>15 gün içerisinde ücretsiz iade. Detaylı bilgi için.Tıklayın.</ListItem>
+    <ListItem>24 saatte kargoda fırsatı iş günlerinde geçerlidir.</ListItem>
+    <ListItem>Modelin Ölçüleri: Boy: 1.75, Göğüs: 80, Bel: 60, Kalça: 88</ListItem>
+    <ListItem>Mankenin üzerindeki ürün S/36 bedendir.</ListItem>
+  </List>
+));
+
+stories.add("No Dot", () => (
+  <List noDot>
+    <ListItem>15 gün içerisinde ücretsiz iade. Detaylı bilgi için.Tıklayın.</ListItem>
+    <ListItem>24 saatte kargoda fırsatı iş günlerinde geçerlidir.</ListItem>
+    <ListItem>Modelin Ölçüleri: Boy: 1.75, Göğüs: 80, Bel: 60, Kalça: 88</ListItem>
+    <ListItem>Mankenin üzerindeki ürün S/36 bedendir.</ListItem>
   </List>
 ));
 
 stories.add("Icon", () => (
-  <List>
-    <Item icon={"search"} dotted={false}>15 gün içerisinde ücretsiz iade. Detaylı bilgi için  </Item>
-    <Item icon={"search"} dotted={false}>24 saatte kargoda fırsatı iş günlerinde geçerlidir.</Item>
-    <Item icon={"search"} dotted={false}>Modelin Ölçüleri: Boy: 1.75, Göğüs: 80, Bel: 60, Kalça: 88</Item>
-    <Item icon={"search"} dotted={false}>Mankenin üzerindeki ürün S/36 bedendir.</Item>
+  <List noDot>
+    <ListItem icon={"search"}>15 gün içerisinde ücretsiz iade. Detaylı bilgi için  </ListItem>
+    <ListItem icon={"search"}>24 saatte kargoda fırsatı iş günlerinde geçerlidir.</ListItem>
+    <ListItem icon={"search"}>Modelin Ölçüleri: Boy: 1.75, Göğüs: 80, Bel: 60, Kalça: 88</ListItem>
+    <ListItem icon={"search"}>Mankenin üzerindeki ürün S/36 bedendir.</ListItem>
   </List>
 ));
