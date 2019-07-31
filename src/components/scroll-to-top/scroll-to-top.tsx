db2c87e9f89442c4754ca5d3dfcda711098ed93f
InanBerkin <berkin.inan1@gmail.com>
2019-07-31 09:06:01 +0300

    Add scroll-to-top-component

---
+++ src/components/scroll-to-top/scroll-to-top.tsx
@@ -0,0 +1,115 @@
+import classNames from "classnames";
+import React, { PureComponent } from "react";
+import IDiv from "../../interfaces/div";
+import "../../styles/components/_scroll-to-top.scss";
+import { detectPassiveEvents } from "../../utils/detectPassiveEvents";
+import Icon from "../icon";
+
+class ScrollToTop extends PureComponent<IScrollToTopProps, IScrollToTopState> {
+  public static defaultProps = {
+    smooth: true,
+  };
+  private requestAnimationFrameId: number | null;
+  private initialPosition: number;
+  private startTime: number | null;
+  private lastScrollPosition: number;
+
+  constructor(props: IScrollToTopProps) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+    this.lastScrollPosition = 0;
+    this.requestAnimationFrameId = null;
+    this.initialPosition = 0;
+    this.startTime = null;
+    this.handleClick = this.handleClick.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.scrollStep = this.scrollStep.bind(this);
+    this.stopScrolling = this.stopScrolling.bind(this);
+  }
+
+  public componentDidMount(): void  {
+    // this.handleScroll();
+    window.document.addEventListener("scroll", this.handleScroll, detectPassiveEvents() ? { passive: true} : false);
+    window.document.addEventListener("wheel", this.stopScrolling, detectPassiveEvents() ? { passive: true } : false);
+    window.document.addEventListener(
+      "touchstart",
+      this.stopScrolling,
+      detectPassiveEvents() ? { passive: true } : false);
+  }
+
+  public componentWillUnmount(): void {
+    window.document.removeEventListener("scroll", this.handleScroll);
+    window.document.removeEventListener("wheel", this.handleScroll, false);
+    window.document.removeEventListener("touchstart", this.handleScroll, false);
+  }
+
+  public render() {
+    const scrollToTopClasses = classNames(
+      "q-scroll-to-top",
+      !this.state.show && "hide",
+      this.props.className,
+    );
+
+    return (
+      <div className={scrollToTopClasses} onClick={this.handleClick}>
+        <Icon name="arrow-top" color="white"/>
+      </div>
+    );
+  }
+
+  private handleClick(): void {
+    this.startTime = null;
+    this.initialPosition = window.pageYOffset;
+    this.requestAnimationFrameId = window.requestAnimationFrame(this.scrollStep);
+  }
+
+  private handleScroll(): void {
+    this.setState({show:
+        window.pageYOffset <= this.lastScrollPosition &&
+        window.pageYOffset > 0,
+    });
+    this.lastScrollPosition = window.pageYOffset;
+  }
+
+  private scrollStep(timestamp: number): void {
+    window.document.removeEventListener("scroll", this.handleScroll);
+    if (!this.startTime) { this.startTime = timestamp; }
+    const scrollPosition = this.easeOutCubic(
+      timestamp - this.startTime!,
+      this.initialPosition,
+      500,
+    );
+    if (window.pageYOffset > 0) {
+      window.scrollTo(0, scrollPosition);
+      this.requestAnimationFrameId = window.requestAnimationFrame(this.scrollStep);
+    } else {
+      this.stopScrolling();
+    }
+  }
+
+  private stopScrolling() {
+    if (this.requestAnimationFrameId) {
+      window.cancelAnimationFrame(this.requestAnimationFrameId);
+      window.document.addEventListener("scroll", this.handleScroll, detectPassiveEvents() ? { passive: true } : false);
+      this.setState({show: false});
+      this.requestAnimationFrameId = null;
+    }
+  }
+
+  private easeOutCubic(currentTime: number, initialPosition: number, duration: number): number {
+    const c = 0 - initialPosition;
+    return c * ((currentTime = currentTime / duration - 1) * currentTime * currentTime + 1) + initialPosition;
+  }
+}
+
+interface IScrollToTopProps extends IDiv {
+  smooth?: boolean;
+}
+
+interface IScrollToTopState {
+  show: boolean;
+}
+
+export default ScrollToTop;
---
+++ src/components/scroll-to-top/test/scroll-to-top.spec.tsx
@@ -0,0 +1,87 @@
+import Enzyme, { mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import faker from "faker";
+import React from "react";
+import sinon, { SinonStub } from "sinon";
+import ScrollToTop from "../scroll-to-top";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+let scrollToStub: SinonStub;
+let requestAnimationFrameStub: SinonStub;
+
+describe("scroll to top specs", () => {
+  const sandbox = sinon.createSandbox();
+
+  beforeEach(() => {
+    window.pageYOffset = 500;
+    scrollToStub = sandbox.stub(window, "scrollTo");
+    requestAnimationFrameStub = sandbox.stub(window, "requestAnimationFrame").returns(faker.random.number());
+  });
+
+  afterEach(() => {
+    sandbox.verifyAndRestore();
+  });
+
+  it("should not be visible on initial rendering", () => {
+    const wrapper = shallow(<ScrollToTop/>);
+
+    expect(wrapper.find(".hide")).toHaveLength(1);
+  });
+
+  it("should be displayed when scrolling up", () => {
+    const wrapper = shallow(<ScrollToTop/>);
+    wrapper.instance().lastScrollPosition = 800;
+    const event = new Event("scroll");
+
+    window.document.dispatchEvent(event);
+
+    expect(wrapper.find(".hide").exists()).toBe(false);
+  });
+
+  it("should not be displayed when scrolling down", () => {
+    const wrapper = shallow(<ScrollToTop/>);
+    wrapper.instance().lastScrollPosition = 300;
+    const event = new Event("scroll");
+
+    window.document.dispatchEvent(event);
+
+    expect(wrapper.find(".hide").exists()).toBe(true);
+  });
+
+  it("should call scrollTo when clicked", () => {
+    const wrapper = shallow(<ScrollToTop/>);
+
+    wrapper.find(".q-scroll-to-top").simulate("click");
+    requestAnimationFrameStub.callArgWith(0, [Date.now()]);
+
+    expect(scrollToStub.called).toBe(true);
+  });
+
+  it("should render correct icon name", () => {
+    const wrapper = shallow(<ScrollToTop/>);
+
+    expect(wrapper.find("Icon").prop("name")).toBe("arrow-top");
+  });
+
+  it("should remove event listeners when unmounted", () => {
+    const wrapper = mount(<ScrollToTop/>);
+    const removeEventListenerStub = sandbox.stub(window, "removeEventListener");
+
+    wrapper.unmount();
+
+    expect(removeEventListenerStub.called).toBe(true);
+  });
+
+  it("should stop scrolling if page Y offset is 0", () => {
+    const wrapper = shallow(<ScrollToTop/>);
+    const stopScrollingStub = sandbox.spy(wrapper.instance(), "stopScrolling");
+    window.pageYOffset = 0;
+
+    wrapper.find(".q-scroll-to-top").simulate("click");
+    requestAnimationFrameStub.callArgWith(0, [Date.now()]);
+
+    expect(stopScrollingStub.called).toBe(true);
+  });
+
+});
