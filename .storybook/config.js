5243079796da3a60833bc07781c7cdf6d2991fd9
antkaynak <antkaynak1@gmail.com>
2019-08-29 11:44:57 +0300

    Remove reboot from storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,6 @@ import { configure } from "@storybook/react";
 import '../src/styles/_globals.scss';
 import '../src/styles/_icon-url.scss';
 import '../src/styles/_font.scss';
-import '../src/styles/_reboot.scss';
 
 const req = require.context("../src/stories", true, /.stories.tsx$/);
 
